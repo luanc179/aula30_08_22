b472217464de59300b01196dd7ade331e4990755
luanc179 <111541122+luanc179@users.noreply.github.com>
2022-08-30 19:59:17 -0300

    commit 30_08

---
+++ TWAcode/script.js
@@ -0,0 +1,15 @@
+const imagens = document.querySelectorAll('img');
+
+console.log(imagens);
+
+imagens.forEach((imagens)=>{
+    console.log(imagens);
+});
+
+const primeiroH2 = document.querySelector('h2');
+
+console.log(primeiroH2);
+
+const ultimoP = document.querySelectorAll('p');
+
+console.log(ultimoP[ultimoP.length-1]);
